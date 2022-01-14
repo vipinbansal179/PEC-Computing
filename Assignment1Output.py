2094c63035a1ea84e9ba5895c36e7d592c0da510
Vipin Bansal <bansalvipin36@gmail.com>
2022-01-14 22:48:25 +0530

    Add files via upload

---
+++ Assignment1Output.py
@@ -0,0 +1,27 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: D:\computing\Assignment1.py =====================
+Enter 1st No = 12
+Enter 2nd No = 12
+Enter 3rd No = 12
+12.0
+Enter Your Gross Income (in nearest integer) = $1000000
+Enter No. Of Dependent = 5
+Your Total Income Tax is : $ 195000.0
+Your SID : 21104111
+Your Name : Vipin Bansal
+For Male (M),For Female(F),For Unknown(U)
+Your Gender : M
+Your Course Name : Electrical
+Your CGPA : 10
+Student Info [21104111, 'Vipin Bansal', 'M', 'Electrical', 10.0]
+Enter marks of 5 students
+10
+20
+30
+40
+50
+[10.0, 20.0, 30.0, 40.0, 50.0]
+Color = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Color = ['Red', 'Green', 'White', 'Purple', 'Yellow']
