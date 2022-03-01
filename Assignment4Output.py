b532960d2faa3e1a379e075119d6d4392925a592
Vipin Bansal <bansalvipin36@gmail.com>
2022-03-01 20:48:54 +0530

    Add files via upload

---
+++ Assignment4Output.py
@@ -0,0 +1,94 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: D:\computing\Assignment4.py =====================
+----------------------------------ASSIGNMENT 4-----------------------------
+
+-------------------------------Question #1----------------------------------
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+---------------------------------Question #2----------------------------------
+ 
+ 
+Enter the number of rows in Pascal's Triangle: 10
+
+Using recursions:
+
+                  1   
+                1   1   
+              1   2   1   
+            1   3   3   1   
+          1   4   6   4   1   
+        1   5   10   10   5   1   
+      1   6   15   20   15   6   1   
+    1   7   21   35   35   21   7   1   
+  1   8   28   56   70   56   28   8   1   
+1   9   36   84   126   126   84   36   9   1   
+
+Using loops:
+
+                  1   
+                1   1   
+              1   2   1   
+            1   3   3   1   
+          1   4   6   4   1   
+        1   5   10   10   5   1   
+      1   6   15   20   15   6   1   
+    1   7   21   35   35   21   7   1   
+  1   8   28   56   70   56   28   8   1   
+1   9   36   84   126   126   84   36   9   1   
+
+---------------------------------Question #3----------------------------------
+ 
+ 
+Enter the first integer: 66
+Enter the second integer: 55
+Remainder is:  11
+Quotient is:  1
+Both values are non zero
+{15}
+{5, 15}
+{16, 5, 15}
+{16, 5, 6, 15}
+{5, 6, 15, 16, 17}
+{5, 6, 7, 15, 16, 17}
+{5, 6, 7, 15, 16, 17}
+Immutable set:  frozenset({16, 17, 5, 6, 7, 15})
+Largest value in the set:  17
+Hash value:  17
+
+---------------------------------Question #4----------------------------------
+ 
+ 
+Object created
+The name of the student it Vipin Bansal and SID is 21104111.
+Object destroyed
+
+---------------------------------QUESTION #5----------------------------------
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+----------------------------------QUESTION #6---------------------------------
+ 
+ 
+Enter the first word: good
+
+Enter a new meaningful word to test your friendship: bad
+The letters aren't exact, friendship is fake!!
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
+----------------------------------ASSIGNMENT OVER-----------------------------
